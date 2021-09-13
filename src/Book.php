7ed5e61078ecca575f4c7081a5f125256f206c77
Christopher Wilkinson <c@wilk.tech>
2021-09-13 17:03:54 +0100

    Remove unused use statements

--- src/Book.php
+++ src/Book.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace TechWilk\BibleVerseParser;
 
-use TechWilk\BibleVerseParser\Validator\BookValidator;
-use TechWilk\BibleVerseParser\Exception\InvalidBookException;
-
 class Book
 {
     protected $name;
