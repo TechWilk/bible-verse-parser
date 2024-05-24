782e19253abc6fa825fb4809cd598b45d82b1a74
Christopher Wilkinson <c@wilk.tech>
2024-05-24 19:06:30 +0100

    WIP bible structure class

---
+++ src/Data/BibleStructure.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TechWilk\BibleVerseParser\Data;
+
+use TechWilk\BibleVerseParser\Book;
+
+class BibleStructure
+{
+    public static function getBibleStructure(): array
+    {
+        $structure = require __DIR__.'/../../data/bibleStructure.php';
+
+        $books = self::convertArrayToBooks($structure);
+
+        return $books;
+    }
+
+    public static function convertArrayToBooks(array $structure): array
+    {
+        $books = [];
+        foreach ($structure as $index => $bookData) {
+            $books[] = new Book(
+                $index,
+                $bookData['number'],
+                $bookData['identifier'],
+                $bookData['name'],
+                $bookData['singularName'] ?? $bookData['name'],
+                $bookData['abbreviations'],
+                $bookData['chapterStructure']
+            );
+        }
+
+        return $books;
+    }
+}
